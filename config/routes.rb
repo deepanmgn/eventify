fbca7a9e01d798041312025f2778c5247bea18fd
Deepan Muruan <deep.mgn@gmail.com>
2015-11-16 17:13:43 +0530

    Scaffold events

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  #get 'home/land'
+  resources :events
+  #get 'new_event_path'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
